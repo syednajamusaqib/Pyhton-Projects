a640180ee250a09c3bf1d7e223cf88c0f2dd2e46
Syed Najam U Saqib <136888817+syednajamusaqib@users.noreply.github.com>
2023-10-08 15:43:46 +0500

    Added comments to  row_echelon.py

--- row_echelon.py
+++ row_echelon.py
@@ -32,6 +32,11 @@ def is_row_echelon_form(matrix):
     return True
     
 # Completing Step 1
+"""
+This function will check first entries of all the rows. If in any of rows first entry is not zero, 
+It means we have the non zero row which can be used as our first row. 
+"""
+
 def find_nonzero_row(matrix, pivot_row, col):
     nrows = matrix.shape[0]
     for row in range(pivot_row, nrows):
@@ -40,19 +45,25 @@ def find_nonzero_row(matrix, pivot_row, col):
     return None
 
 # Completing Step 2
-
+# Swapping rows so that we can have our non zero row on the top of the matrix
 def swap_rows(matrix, row1, row2):
     matrix[[row1, row2]] = matrix[[row2, row1]]  
 
 # Completing Step 3
-
+"""
+We will make our pivot element 1 by simply dividing(Floor division) our pivot_row
+(Row where we are currently performing operations) by pivot_element
+"""
 def make_pivot_one(matrix, pivot_row, col):
     pivot_element = matrix[pivot_row, col]
     matrix[pivot_row] //= pivot_element
     # print(pivot_element)
 
 # Completing Step 4
-
+"""
+When our pivot_element is set to 1, the next step is to make all the elements below 
+pivot_element 0.
+"""
 def eliminate_below(matrix, pivot_row, col):
     nrows = matrix.shape[0]
     pivot_element = matrix[pivot_row, col]
@@ -68,7 +79,7 @@ def row_echelon_form(matrix):
     nrows = matrix.shape[0]
     ncols = matrix.shape[1]
     pivot_row = 0
-
+# this will run for number of column times. If matrix has 3 columns this loop will run for 3 times
     for col in range(ncols):
         nonzero_row = find_nonzero_row(matrix, pivot_row, col)
         if nonzero_row is not None:
