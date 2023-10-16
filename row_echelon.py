e0eaa5c05a44e254efc340dcec2ff48996542989
Syed Najam U Saqib <136888817+syednajamusaqib@users.noreply.github.com>
2023-10-16 19:01:05 +0500

    Updated row_echelon.py

--- row_echelon.py
+++ row_echelon.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np 
 
 """
 There are basically four steps in this approach
@@ -6,80 +6,63 @@ Step 1: Check for Non Zero Row
 Step 2: Swap Rows
 Step 3: Make Pivot element of Pivot Row "1"
 Step 4: Eliminate elements below pivots to get Row-Echelon Form  
+
 """
 
-# Function to check if matrix is in REF
-
-def is_row_echelon_form(matrix):
-    if not matrix.any():
+# Function to check whether matrix is zero matrix or not
+def is_zero_matrix(matrix):
+    rows = len(matrix)
+    if rows == 0:
         return False
-    
-    rows = matrix.shape[0]
-    cols = matrix.shape[1]
-    prev_leading_col = -1
-
+    cols = len(matrix[0])
+    if cols == 0:
+        return False
     for row in range(rows):
-        leading_col_found = False
         for col in range(cols):
-            if matrix[row,col] != 0:
-                if col <= prev_leading_col:
-                    return False
-                prev_leading_col = col
-                leading_col_found = True
-                break
-        if not leading_col_found and any(matrix[row,col] != 0 for col in range(cols)):
-            return False
+            if matrix[row,col] !=0:
+                return False
     return True
-    
-# Completing Step 1
-"""
-This function will check first entries of all the rows. If in any of rows first entry is not zero, 
-It means we have the non zero row which can be used as our first row. 
-"""
 
+# Function to check for empty matrix
+def is_empty(matrix):
+    rows = len(matrix)
+    if rows == 0:
+        return True
+    cols = len(matrix[0])
+    if cols == 0:
+        return True
+
+# Function to Find non zero row of the matrix
 def find_nonzero_row(matrix, pivot_row, col):
-    nrows = matrix.shape[0]
+    nrows = len(matrix)
     for row in range(pivot_row, nrows):
         if matrix[row, col] != 0:
             return row
     return None
 
-# Completing Step 2
-# Swapping rows so that we can have our non zero row on the top of the matrix
+# Function to swap non zero row with the pivot row
 def swap_rows(matrix, row1, row2):
     matrix[[row1, row2]] = matrix[[row2, row1]]  
 
-# Completing Step 3
-"""
-We will make our pivot element 1 by simply dividing(Floor division) our pivot_row
-(Row where we are currently performing operations) by pivot_element
-"""
+# This function will make pivot element to be 1
 def make_pivot_one(matrix, pivot_row, col):
     pivot_element = matrix[pivot_row, col]
     matrix[pivot_row] //= pivot_element
-    # print(pivot_element)
 
-# Completing Step 4
-"""
-When our pivot_element is set to 1, the next step is to make all the elements below 
-pivot_element 0.
-"""
+# Function to eliminate all the entries below pivot element to be 0
 def eliminate_below(matrix, pivot_row, col):
-    nrows = matrix.shape[0]
+    nrows = len(matrix)
     pivot_element = matrix[pivot_row, col]
     for row in range(pivot_row + 1, nrows):
         factor = matrix[row, col]
-        # print(factor)
         matrix[row] -= factor * matrix[pivot_row]
-        # print(matrix[row])
 
-# Implementing above functions
-
+# Converting matrix into row echelon form by implementing above functions
 def row_echelon_form(matrix):
-    nrows = matrix.shape[0]
-    ncols = matrix.shape[1]
+    rows = len(matrix)
+    ncols = len(matrix[0])
     pivot_row = 0
-# this will run for number of column times. If matrix has 3 columns this loop will run for 3 times
+
     for col in range(ncols):
         nonzero_row = find_nonzero_row(matrix, pivot_row, col)
         if nonzero_row is not None:
@@ -87,22 +70,66 @@ def row_echelon_form(matrix):
             make_pivot_one(matrix, pivot_row, col)
             eliminate_below(matrix, pivot_row, col)
             pivot_row += 1
-    print(matrix)
     return matrix
 
-# Generating Random matrices as Test cases
-# It is optional to use this function
-def generate_random_matrix(rows, cols, min_value, max_value):
-    return np.random.randint(min_value, max_value + 1, size=(rows, cols), dtype=int)
+
+# This function will check if a matrix is in row echelon form or not
+def is_echelon_form(matrix):
+    if not matrix.any():
+        return False  # Empty matrix is not in echelon form
+    if is_zero_matrix(matrix):
+        return True
+    
+    if len(matrix) == 1:  # matrix with 1 length is already in row echelon form
+        return True
+    rows = len(matrix)
+    cols = len(matrix[0])
+    prev_leading_col = -1  # Initialize to an invalid value
+
+    for i in range(rows):
+        leading_col_found = False
+
+        for j in range(cols):
+            if matrix[i][j] != 0:
+                if j <= prev_leading_col:
+                    return False  # Leading non-zero element is not to the right
+                prev_leading_col = j
+                leading_col_found = True
+                break
+
+        if not leading_col_found and any(matrix[i][j] != 0 for j in range(cols)):
+            return False  # All elements after the leading zeros should be zero
+
+    return True
+
+# Taking input from the user
+
+rows = abs(int(input("Enter number of rows: "))) # Taking number of rows as input
+cols = abs(int(input("Enter number of columns: "))) # Taking number of columns as input
+matrix = [] # Initializing empty list so that later on we can append lists into it to make it matrix
+for i in range(rows):
+    row = []
+    for col in range(cols):
+        number = float(input())
+        row.append(number) # Appending number to the row
+    matrix.append(row) # Appending to list
+matrix_np = np.array(matrix) # Converting into numpy array
 
 
-matrix = np.array([[5,4,0,2],[10,2,5,9],[0,0,0,0]])
-# for i in range(100):
-    # matrix = generate_random_matrix(3,4,0,99)
-result = row_echelon_form(matrix)
-if is_row_echelon_form(result):
-        print("In REF")
+if is_zero_matrix(matrix_np): # Checking if matrix is zero matrix
+    print("Zero matrix is already in row echelon form")
+elif is_empty(matrix_np):
+    print("Empty Matrix is not in Row echelon form")
 else:
-        print("Not in REF--------------->")
+    print(f"Before Converting into REF \n {matrix_np}")
+    result = row_echelon_form(matrix_np)
+    print(f"After Converting into REF \n {result}")
+    if is_echelon_form(result):
+        print("In REF")
+    else:
+        print("Not in REF")
 
-# print(result)
+
+
+
+
